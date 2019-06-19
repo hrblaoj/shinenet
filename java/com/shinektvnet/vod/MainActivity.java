75af9df5b19688d6e7ef383cd9576a53adc95bcb
hrblaoj <hbb520jg@126.com>
2019-06-19 17:34:57 +0800

    fix

--- java/com/shinektvnet/vod/MainActivity.java
+++ java/com/shinektvnet/vod/MainActivity.java
@@ -26,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
         functionpanel = new MyFunctionpannelF().create();
         setContentView(functionpanel.getView());
 
-
     }
 
     /**
